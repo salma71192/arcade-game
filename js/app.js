a1b9de84c0942c0d39fdbe15a26e0c0fb8fab4a5
Salma Mohammed <sm.71175@gmail.com>
2017-12-29 22:11:21 +0200

    Refactor app.js: Use Classes

--- js/app.js
+++ js/app.js
@@ -1,106 +1,120 @@
+class Subject {
+    constructor(x, y, speed, img) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = img
+    }
+
+    // Update the subject's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x += this.speed * dt;
+    }
+
+    // Draw the subject on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+};
+
+
 // Enemies our player must avoid
-let Enemy = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    this.x += this.speed * dt;
-    if (this.x >= 505) {
-        this.x = 0;
+class Enemy extends Subject {
+    constructor(x, y, speed, img = "images/enemy-bug.png") {
+        super(x, y, speed, img);
+        this.sprite = img;
     }
-    // collision function
-    collision(this);
-};
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Player class
-let Player = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/char-boy.png';
+    update(dt) {
+        super.update(dt);
+        if (this.x >= 505) {
+            this.x = 0;
+        }
+        // collision method
+        player.collision(this);
+    }
 }
 
-Player.prototype.update = function(dt) {   
-    //this.x = this.speed * dt;
-};
-
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.reset = function() {
-    this.x = 200;
-    this.y = 408;
-};
-
-let score = 1;
-// display player key moves and make sure not go beyond the boundries
-Player.prototype.handleInput = function(keyPress) {
-    if (keyPress == 'left') {
-        player.x -= player.speed + 18;
-        if(player.x < 0) {
-            player.x = 0;
-        }
-    } else if(keyPress == 'right') {
-        player.x += player.speed + 18;
-        if(player.x > 408) {
-            player.x = 408;
-        }
-    } else if(keyPress == 'up') {
-        player.y -= player.speed;
-        if(player.y <= -7) {
-            player.y = -7;         
-            setTimeout(() => {
-                player.reset();
-                levelOfDifficulty();
-            }, 700);
-                
-        }
-    } else if(keyPress == 'down') {
-        player.y += player.speed;
-        if(player.y > 408) {
-            player.y = 408;
-        }
-    }
-};
-
-
-
-let scoreLabel = document.getElementById('score'),
+// array to collect enemies
+let allEnemies = [],
+    scoreLabel = document.getElementById('score'),
     livesLabel = document.getElementById('lives'),
-    lives = 4;
-// check collision between player and enemy
-function collision(enemy) {
-    if (player.y + 131 >= enemy.y + 90
-        && player.x + 25 <= enemy.x + 88
-        && player.y + 73 <= enemy.y + 100
-        && player.x + 76 >= enemy.x + 11) {
-        player.x = 200;
-        player.y = 408;
-        // decrease player lives
-        lives--;
+    lives = 4,
+    score = 1;
+
+// Our player
+class Player extends Subject {
+    constructor(x, y, speed, img = "images/char-boy.png") {
+        super(x, y, speed, img);
+        this.sprite = img;
     }
-    // player loses if lives = 0
-    if(lives === 0) {
-        alert('you Lose!');
-        // Reset the game
+
+    update(dt) {}
+
+    // reset game to original state
+    reset() {
         lives = 4;
-        score = 0;
+        score = 1;
+        allEnemies = [];        
+        enemy = new Enemy(0, Math.random() * 180 + 50, Math.random() * 200);
+        allEnemies.push(enemy);        
     }
-    
-    livesLabel.innerHTML = `Lives: ${lives}`;
+
+
+    // display player key moves and make sure not go beyond the boundries
+    handleInput(keyPress) {
+        if (keyPress == 'left') {
+            this.x -= 101;
+            if(this.x < 0) {
+                this.x = 0;
+            }
+        } else if(keyPress == 'right') {
+            this.x += 101;
+            if(this.x > 408) {
+                this.x = 408;
+            }
+        } else if(keyPress == 'up') {
+            this.y -= 83;
+            if(this.y <= -7) {
+                this.y = -7;         
+                setTimeout(() => {
+                    this.y = 408;
+                    levelOfDifficulty();
+                }, 700);
+                    
+            }
+        } else if(keyPress == 'down') {
+            this.y += 83;
+            if(this.y > 408) {
+                this.y = 408;
+            }
+        }
+    }
+
+    // check collision between player and enemy
+    collision(enemy) {
+        if (this.y + 131 >= enemy.y + 90
+            && this.x + 25 <= enemy.x + 88
+            && this.y + 73 <= enemy.y + 100
+            && this.x + 76 >= enemy.x + 11) {
+            this.x = 200;
+            this.y = 408;
+            // decrease player lives
+            lives--;
+        }
+        // player loses if lives = 0
+        if(lives === 0) {
+            alert('you Lose!');
+            // Reset the game
+            this.reset();
+        }
+        
+        livesLabel.innerHTML = `Lives: ${lives}`;
+        scoreLabel.innerHTML = `Score: ${score}`;
+    }
 }
+
 
 // increase score and difficulty each time player reach water
 function levelOfDifficulty() {
@@ -113,13 +127,12 @@ function levelOfDifficulty() {
             enemy = new Enemy(0, Math.random() * 180 + 50, Math.random() * 200);
             allEnemies.push(enemy);
         }
-
-    // player end the game
-    } else if (score === 8) {
+    }
+    // Player wins
+    if (score === 8) {
         alert('You won');
         // reset the game to start
-        score = 0;
-        lives = 4;
+        player.reset();
     }
 
     scoreLabel.innerHTML = `Score: ${score}`;
@@ -127,12 +140,10 @@ function levelOfDifficulty() {
 
 // new played intance
 let player = new Player(200, 408, 83);
-// array to collect enemies
-let allEnemies = [];
+
 // new enemy instance
 let enemy = new Enemy(0, Math.random() * 180 + 50, Math.random() * 200);
 allEnemies.push(enemy);
-
 
 
 // This listens for key presses and sends the keys to your
@@ -147,5 +158,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
