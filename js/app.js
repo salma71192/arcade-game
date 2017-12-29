4a2ab90507664087318fcdda774b835e01e51dc8
Salma Mohammed <sm.71175@gmail.com>
2017-12-29 08:49:17 +0200

    Add player handout method.

--- js/app.js
+++ js/app.js
@@ -43,3 +43,51 @@ Player.prototype.reset = function() {
     this.x = 200;
     this.y = 408;
 };
+
+let score = 1;
+// display player key moves and make sure not go beyond the boundries
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x -= player.speed + 18;
+        if(player.x < 0) {
+            player.x = 0;
+        }
+    } else if(keyPress == 'right') {
+        player.x += player.speed + 18;
+        if(player.x > 408) {
+            player.x = 408;
+        }
+    } else if(keyPress == 'up') {
+        player.y -= player.speed;
+        if(player.y <= -7) {
+            player.y = -7;         
+            setTimeout(() => {
+                player.reset();
+                levelOfDifficulty();
+            }, 700);
+                
+        }
+    } else if(keyPress == 'down') {
+        player.y += player.speed;
+        if(player.y > 408) {
+            player.y = 408;
+        }
+    }
+};
+
+
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
