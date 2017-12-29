0aec155be6bf1e67d14a7af7de36eae7a37d20a8
Salma Mohammed <sm.71175@gmail.com>
2017-12-29 08:35:57 +0200

    Add enemy and player classes

---
+++ js/app.js
@@ -0,0 +1,45 @@
+// Enemies our player must avoid
+let Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+    if (this.x >= 505) {
+        this.x = 0;
+    }
+    // collision function
+    collision(this);
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Player class
+let Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {   
+    //this.x = this.speed * dt;
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 408;
+};
